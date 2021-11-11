341e92c3ca6070f7748ac0c35d0956d2d105f54e
nexosmart-violet <teamviolet@nexosmart.com.ar>
2021-11-11 18:34:30 -0300

    microservices functionality

---
+++ ms-b/B.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require("http");
+
+const app = express();
+
+const PORT = process.env.PORT;
+const A_HOST = process.env.A_HOST;
+const A_PORT = parseInt(process.env.A_PORT);
+
+app.get('/b', (req, res) => {
+    var options = {
+        host: A_HOST,
+        port: A_PORT,
+        path: '/a',
+        method: 'GET',
+        headers: req.headers,
+    };
+
+    const wordreq = http.request(options, 
+        wordres => {
+            let json = [];
+            wordres.on('data', d => {
+                json.push(d);
+            });
+            wordres.on('end', () => {
+                json = JSON.parse(json);
+                let capitalized = json.word.toUpperCase();
+                res.writeHead(200, {
+                    "Content-Type":"application/json",
+                });
+                res.send(200, `{"cap" : ${capitalized}}`);
+            });
+
+        });
+    wordreq.on('error', (e) =>{
+        console.log('problem with request: '+e.message);
+    });
+    wordreq.end();
+});
+
+app.listen(PORT, () => {
+    console.log('random word microservice');
+});
---
+++ ms-c/C.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require("http");
+
+const app = express();
+
+const PORT = process.env.PORT;
+const B_HOST = process.env.B_HOST;
+const B_PORT = parseInt(process.env.B_PORT);
+
+app.get('/c', (req, res) => {
+    var options = {
+        host: B_HOST,
+        port: B_PORT,
+        path: '/a',
+        method: 'GET',
+        headers: req.headers,
+    };
+
+    const wordreq = http.request(options, 
+        wordres => {
+            let json = [];
+            wordres.on('data', d => {
+                json.push(d);
+            });
+            wordres.on('end', () => {
+                json = JSON.parse(json);
+                let str = json.cap.concat(random(1,101));
+                res.writeHead(200, {
+                    "Content-Type":"application/json",
+                });
+                res.send(200, `{"str" : ${str}}`);
+            });
+
+        });
+    wordreq.on('error', (e) =>{
+        console.log('problem with request: '+e.message);
+    });
+    wordreq.end();
+});
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+app.listen(PORT, () => {
+    console.log('random word microservice');
+});
